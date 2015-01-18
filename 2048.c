b96d9987a8b4ec2a0c9e3a59a9680503cef312ff
Ghobadimhd <Ghobadi.mhd@outlook.com>
2015-01-18 10:55:28 +0330

    comment unImplimented function , impliment newTile function , little bug fix

--- 2048.c
+++ 2048.c
@@ -6,6 +6,8 @@ Email : ghobadi.mhd@outlook.com
 describtion : This is cool game  .
 */
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 #define up 'A'
 #define down 'B'
 #define right 'C'
@@ -25,17 +27,50 @@ int main(int argc, const char *argv[])
 	char direction;
 	newTile();
 	newTile();
-	show();
+	//show();
 	while (gameNotOver) 
 	{
-		scanf(" %c%c%c",&move,&move,&move);// it get three char esc(27), [ , and arrow char . we need last one 
-		move(direction);
-		merge(direction);
-		move(direction);
-		newTile();
-		show() ;
-		gameNotOver = isLose() ;
+		scanf(" %c%c%c",&direction,&direction,&direction);// it get three char esc(27), [ , and arrow char . we need last one 
+		//move(direction);
+		//merge(direction);
+		//move(direction);
+		//newTile();
+		//show() ;
+		//gameNotOver = isLose() ;
 	}
 	
 	return 0;
+}
+/*
+this is function creating new tile  .
+*/
+void newTile()
+{
+	int isFull=1 ,  i , j // i and j are used for index's 
+			, *tiles = screen ; // tiles get us access to screen in linear way 
+	
+	// it's look for one free tile to prove that screen is not full 
+	// it saw array as One-dimensional for decreasing loops 
+	for (i = 0; i < 16; i++) 
+		if (tiles[i]==0 )
+		{
+			isFull=0;
+			break ; 
+		}
+
+	if ( !isFull )
+	{
+		// looking for a random free tile for new Tile 
+		do 
+		{
+			i=rand() % 4;
+			j=rand() % 4;
+		} while (screen[i][j]!= 0);
+	int randomPower = rand() % 2  + 1 
+		, newTileValue = pow(2,randomPower);// tile value should be power of 2 , and almost 2 and 4 
+
+	// random tile appeared on screen 
+	screen[i][j]=newTileValue ; 
+	}
+	
 }
